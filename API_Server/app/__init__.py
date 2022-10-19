c223e45bc2dc7bdbe404934da7a3747c4f7f3221
saanikajoshi <spj29@njit.edu>
2022-10-19 12:45:05 -0400

    updates

--- API_Server/app/__init__.py
+++ API_Server/app/__init__.py
@@ -1,2 +1,12 @@
 """This is the app used to test the api aspect"""
+# setup is if Safrs is being used
 
+# this is where the imports will be
+
+# database connection
+
+# This is where sqla database objects will be
+
+# api endpoint creation
+
+# app creation
