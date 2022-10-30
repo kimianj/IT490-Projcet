9baef0bc6e200e8f992b54a197d378c24dd25c21
David Gustavson <dg526@njit.edu>
2022-10-30 19:17:33 -0400

    Branched out db code to multiple files to improve organization & reduce Spaghetti-ness. Also made database more secure, added 'email' table to users table, and hashed passwords.

--- Database_Server/databaseServer.php
+++ Database_Server/databaseServer.php
@@ -1,35 +1,60 @@
 #!/usr/bin/php
 <?php
 require_once('path.inc');
-require_once('get_host_info.inc');
+require_once('getHostInfo.inc');
 require_once('rabbitMQLib.inc');
+require_once('getDatabase.inc');
+require_once('login-register.inc');
+/*
+function getDB(){
+	$dbc;
+	$dbc = parse_ini_file("dbLogin.ini",$process_sections=true);
+	$db = new mysqli('localhost', $dbc["login"]["USER"],$dbc["login"]["PASSWORD"],$dbc["login"]["DATABASE"]);
+	return $db;
+}
+function createNewUser($username, $email, $password){
+	$password = password_hash($password, PASSWORD_BCRYPT);
+	$db = getDB();
+	$q1 = "SELECT userid from users where (username='$username' or email='$username')";
+	$result = $db->query($q1);
+    if($result->num_rows > 0){
+    	echo "User already exists.".PHP_EOL;
+    	return false;
+    }
+    $q2 = "INSERT INTO users (username, email, password) VALUES ('$username','$email','$password')";
+	$result = $db->query($q2);
+	if($result){
+		echo "Created new user.".PHP_EOL;
+		return true;
+	}
+	return false;
+}
 
 function doLogin($username,$password)
 {
     // lookup username in database
     // check password
-    $mydb = new mysqli('localhost', 'testUser','12345','db490');
-    /*
-    if($mydb->errno != 0)
-	{
-		echo "failed to connect to database: ". $mydb->error . PHP_EOL;
-		return false;
-	}
-	*/
-    $query = "SELECT userid, username from users where username='$username' and password='$password'";
+    $mydb = getDB();
+    $query = "SELECT userid, username, password from users where (username='$username' or email='$username')";
     $result = $mydb->query($query);
     if($result->num_rows > 0){
-    	echo "Found user".PHP_EOL;
-    	return true;
+    	if(password_verify($password, $result->fetch_row()[2])){
+    		echo "Found user.".PHP_EOL;
+    		return true;
+    	}
+    	else{
+    		echo "Incorrect password.".PHP_EOL;
+    		return false;
+    	}
     }
     else{
-    	echo "Could not find user".PHP_EOL;
+    	echo "Incorrect username/email.".PHP_EOL;
     	return false;
     }
     //return false if not valid
-}
+}*/
 function makeNewSession(){
-	$sdb = new mysqli('localhost', 'testUser','12345','db490');
+	$sdb = getDB();
 	if($sdb->errno != 0)
 	{
 		echo "failed to connect to database: ". $mydb->error . PHP_EOL;
