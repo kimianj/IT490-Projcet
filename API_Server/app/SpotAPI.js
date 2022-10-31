3c64007971fa9bf324d6ee51b7a553c7afa44fbe
saanikajoshi <spj29@njit.edu>
2022-10-31 16:54:50 -0400

    started API process

--- API_Server/app/SpotAPI.js
+++ API_Server/app/SpotAPI.js
@@ -1,3 +1,19 @@
 const API = (function () {
+    const clientID = "326304fb776249b09066a335fa56c207";
+    const clientSecret = "3a6a1810dd548d386417a57a8b32afd";
 
+    const getToken = async () => {
+
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded',
+                'Authorization' : 'Basic ' + btoa(clientID + ':' + clientSecret)
+            },
+            body: 'type=clientcreds'
+        });
+
+        const data = await result.json();
+        return data.accessToken;
+    }
 })
