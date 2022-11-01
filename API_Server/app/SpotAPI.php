0c8dd8000598bc58528efc58788c7f6449850afa
saanikajoshi <spj29@njit.edu>
2022-11-01 19:24:20 -0400

    fixed comments

--- API_Server/app/SpotAPI.php
+++ API_Server/app/SpotAPI.php
@@ -21,18 +21,18 @@ class Spotify {
         $authLink = 'https://accounts.spotify.com/authorize?' . http_build_query( $data );
     }
 
-<!--     function getUserPlaylist ($client) { -->
-<!--         $curl = curl_init(); -->
-<!--         curl_setopt ($curl, CURLOPT_URL, $urlUP); -->
-<!--         curl_setopt ($curl, CURLOPT_HEADER, ) -->
-<!--   --header 'Authorization: ' \ -->
-<!--   --header 'Content-Type: application/json' -->
-<!--     } -->
+ /*   function getUserPlaylist ($client) {
+        $curl = curl_init();
+        curl_setopt ($curl, CURLOPT_URL, $urlUP);
+        curl_setopt ($curl, CURLOPT_HEADER, )
+  --header 'Authorization: ' \
+  --header 'Content-Type: application/json'
+    }
 
-<!-- curl --request GET \ -->
-<!--   --url https://api.spotify.com/v1/users/user_id/playlists \ -->
-<!--   --header 'Authorization: ' \ -->
-<!--   --header 'Content-Type: application/json' -->
+curl --request GET \
+  --url https://api.spotify.com/v1/users/user_id/playlists \
+  --header 'Authorization: ' \
+  --header 'Content-Type: application/json' */
 }
 
 function requestProcessor($request) {
