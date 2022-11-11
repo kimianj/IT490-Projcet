36d6a578b794fc4c71f92303889e60616c4816e5
saphire001 <spj29@njit.edu>
2022-11-11 23:47:36 +0000

    added events function and edited others

--- API_Server/app/SpotAPI.php
+++ API_Server/app/SpotAPI.php
@@ -38,32 +38,61 @@ require_once('rabbitMQLib.inc');
             }
     }
 
-    function search() {
-        $request["artist"] = $artist;
-        $request["title"] = $title;
+    function search($title) {
 
         $curl = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://api.spotify.com/v1/search?q='.urlencode($artist.' '.$title).'&type=track');
+        curl_setopt($curl, CURLOPT_URL, 'https://api.spotify.com/v1/search?q='.urlencode($title).'&type=track');
         curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, array( 'Content-Type:application/json', 'Authorization: Bearer ' . $_GET['access'] ) );
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt( $curl, CURLOPT_HTTPHEADER, array( 'Content-Type:application/json', 'Authorization: Bearer ' . $_GET['access'] ) );
 
          $response = curl_exec($curl);
          curl_close( $curl );
          return array("response" => $response);
     }
 
-    function getRecomendation() {
-        $request["genre"] = $genre;
+    function getRecomendation($tracks) {
+    	$trackList = "";
+    	for ($i = 0; $i<count($tracks); $i+=1) {
+    		if (i > 0) {
+    			$trackList = $trackList.","; 
+    		}
+    		$trackList = $trackList.$tracks[$i]; 
+    	}
+    
         $curl = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://api.spotify.com/v1/recommendations?'.urlencode($genre));
+        curl_setopt($curl, CURLOPT_URL, 'https://api.spotify.com/v1/recommendations?seed_tracks='.urlencode($trackList).'&market=US');
         curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, array( 'Content-Type:application/json', 'Authorization: Bearer ' . $_GET['access'] ) );
+        curl_setopt( $curl, CURLOPT_HTTPHEADER, array( 'Content-Type:application/json', 'Authorization: Bearer ' . $_GET['access'] ) );
 
         $response = curl_exec($curl);
         curl_close( $curl );
         return array("response" => $response);
+    }
+    
+    function artistEvents($artist) {
+
+	$curl = curl_init();
+
+	curl_setopt_array($curl, [
+		CURLOPT_URL => "https://concerts-artists-events-tracker.p.rapidapi.com/artist?name=".urlencode($artist),
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_HTTPHEADER => [
+			"X-RapidAPI-Host: concerts-artists-events-tracker.p.rapidapi.com",
+			"X-RapidAPI-Key: 413f8728ffmsh3d4c4e519c045abp10714bjsn818deccfc438"
+		],
+	]);
+
+	$response = curl_exec($curl);
+	curl_close($curl);
+	return array("response" => $response);
     }
 
 function requestProcessor($request) {
@@ -79,10 +108,12 @@ function requestProcessor($request) {
         return getAccTok();
         break;
     case "search":
-        return search();
+        return search($request["title"]);
         break;
     case "recommendations":
-        return getRecomendation();
+        return getRecomendation($request["tracks"]);
+    case "events": 
+    	return artistEvents($request["name"]); 
   }
   return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
